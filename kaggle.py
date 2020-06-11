e9499bfe42489c1d83d97a3e6ea8856cb576a853
Felix Fernando <54938467+voltgizerz@users.noreply.github.com>
2020-06-11 16:22:32 +0700

    filter substring in groups

--- kaggle.py
+++ kaggle.py
@@ -1,5 +1,7 @@
 import csv
 import os
+
+
 # OPEN LIST CATEGORY CSV
 with open('./list.csv', 'r') as f:
     reader = csv.reader(f)
@@ -22,18 +24,20 @@ print(dataSpam[1])
 print(dataSpam[1][1])
 
 os.remove('./output.csv')
-with open('./output.csv', 'a', newline='') as f: # INSER DATA OUTPUT
+with open('./output.csv', 'a', newline='') as f:  # INSER DATA OUTPUT
     writer = csv.writer(f)
-    writer.writerow(["index","groups_found"])
-    
-i = 0 
+    writer.writerow(["index", "groups_found"])
+
+i = 0
 x = 0
-j = 0 
-storeFound = [] #SAVES LIST GROUP FOUNDED
-for fullspam in dataSpam:
-    fullstring = dataSpam[j+1][1].lower()
+index = 0
+storeFound = []  # SAVES LIST GROUP FOUNDED
+categoryFound = []
+for fullspam in range(len(dataSpam)-1):
+    fullstring = dataSpam[index+1][1].lower()
     x = 0
     storeFound = []
+    categoryFound = []
     count = 0
     for fullcategory in dataCat:
         i = 0
@@ -41,16 +45,42 @@ for fullspam in dataSpam:
         for category in splitCat:
             if splitCat[i] in fullstring:
                 simpan = dataCat[x][0]
+                categoryFound.insert(count, splitCat[i])
                 storeFound.insert(count, int(simpan))
             else:
                 pass
             i += 1
             count += 1
         x += 1
-    print("Index : ", j," - Group : ", list(dict.fromkeys(storeFound)))
-    with open('./output.csv', 'a', newline='') as f: # INSER DATA OUTPUT
-        writer = csv.writer(f)
-        writer.writerow([j,list(dict.fromkeys(storeFound))])
-    j += 1
+        #DO FILTER
 
-# TO DO FIX ALL SUBSTRING  
+    listDel = [] #STORE GROUP SUBSTRINGS WANT TO DELETED
+    h = 0
+    for cat in categoryFound:
+        for j in range(len(categoryFound)-1):
+            if (cat in categoryFound[j] or categoryFound[j] in cat) and len(cat) <= len(categoryFound[j]):
+                pass
+            elif (cat in categoryFound[j] or categoryFound[j] in cat) and len(cat) > len(categoryFound[j]):
+                listDel.insert(j, int(j))
+            elif categoryFound[j] not in cat or cat not in categoryFound[j] :
+                pass
+            h += 1
+    # CONVERT LISTDEL DELETE DATA MULTIPLE
+    listDel = list(dict.fromkeys(listDel))
+    #print(listDel)
+    #DO POP storeFound
+    for i in range(len(listDel)-1):
+        storeFound.pop(listDel[i])
+        categoryFound.pop(listDel[i])
+
+    # CONVERT TO DICT FILTER SAME GROUPS
+    print("Index : ", index, " | Group : ", list(dict.fromkeys(storeFound)))
+    print("Nama Category : ", categoryFound)
+    #print("\n")
+    with open('./output.csv', 'a', newline='') as f:  # INSER DATA OUTPUT
+        writer = csv.writer(f)
+        writer.writerow([index, list(dict.fromkeys(storeFound))])
+    index += 1
+
+print("SUCCESSFULLY OUTPUTED : ", index, "DATA! ")
+# TO DO FIX ALL SUBSTRING
